032d7a5ac25ab24991887ea005690f44a4f42d19
KAJI <kajiharalivi@icloud.com>
2022-06-06 18:32:59 +0900

    feat:メニューアイコンの実装

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { Home } from "./pages/home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About } from "./pages/about";
+import { Products } from "./pages/products";
+import { History } from "./pages/history";
+import { useEffect } from "react";
+
+
+export const App = () => {
+  
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="history" element={<History />} />
+          <Route path="products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
